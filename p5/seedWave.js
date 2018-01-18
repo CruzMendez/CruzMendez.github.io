5339288358a73fc7857ed3aa3b7742bea065a4d9
CruzMendez <camendez@utexas.edu>
2018-01-18 10:58:43 -0600

    updated
    added wave code

--- p5/seedWave.js
+++ p5/seedWave.js
@@ -1,87 +1,63 @@
-var seed;
+var canvas;
+var seedCount = 30;
+var seedOrigin = 0;
+var seeds = [];
+var currentPage;
+var pageTitle = ["services", "skills", "samples", "about"];
+var subTitle;
+//var amplitude = .4;
+//var period = 100;
 
 
 function setup() {
-    createCanvas(innerWidth,innerHeight); 
-//    canvas.position(400,500);
-//    background(0);
-//    createButton();
+    canvas = createCanvas(innerWidth,200); 
+//    canvas.position(0,300);
+	frameRate(40);
+    for (var i=0; i<seedCount; i++) {
+        seedOrigin = seedOrigin + 25;
+        seeds[i] = new Seed();
+    };
+    
 }
 
 function draw() {
-    background(0);    
-    fill(150,0,255);
-    stroke(255);
-    noStroke();
-    ellipse(100,100,50,50);
-//    if (true) {
-//        stroke(random(0,255),random(0,255),random(0,255));
-//    line(mouseX, mouseY, pmouseX, pmouseY);
-//    };
-}
+	background(0);
+	textSize(35);
+	textFont('Helvetica');
+	fill(179,114,201);
+	noStroke();
+	text(pageTitle[0],50,45);
+	for (var i=0; i<seeds.length; i++) {
+		var positionY = this.seeds[i].y;
+		seeds[i].display();
+		seeds[i].waveDown();
+//		if (positionY === 120) {
+//			seeds[i].waveUp();
+//		};
+	};   
+	stroke(125);
+	line(seeds[5].x,seeds[5].y,200,33);
+	noStroke();
+	fill(179,114,201,80);
+	ellipse(seeds[5].x,seeds[5].y,40,40)
+};
 
-//var xspacing = 35;   // How far apart should each horizontal location be spaced
-//var w = 0;              // Width of entire wave
-//var maxwaves = 5;   // total # of waves to add together
-//
-//var theta = 0.0;
-//var amplitude = []; //float[maxwaves];   // Height of wave
-////float[] dx = new float[maxwaves];          // Value for incrementing X, to be calculated as a function of period and xspacing
-//var yvalues = [];                           // Using an array to store height values for the wave (not entirely necessary)
-//var ellipseCenter = [];
-//
-//function setup() {
-//  createCanvas(innerWidth,200);
-//  frameRate(20);
-////  colorMode(RGB, 255, 255, 255, 100);
-//  w = width + 10;
-//
-//  for (var i = 0; i < maxwaves; i++) {
-//    amplitude[i] = random(10,20);
-//    var period = random(100,300); // How many pixels before the wave repeats
-//    dx[i] = (TWO_PI / period) * xspacing;
-//  }
-//
-//  yvalues = w/xspacing;
-//}
-//
-//function draw() {
-//  background(0); //color
-//  calcWave();
-//  renderWave();
-//}
-//
-//function calcWave() {
-//  theta += 0.02; // Increment theta (try different values for 'angular velocity' here
-//  for (var i = 0; i < yvalues.length; i++) {  // Set all height values to zero
-//    yvalues[i] = 0;
-//  }
-//
-//  // Accumulate wave height values
-//  for (var j = 0; j < maxwaves; j++) {
-//    var x = theta;
-//    for (var i = 0; i < yvalues.length; i++) {
-//      // Every other wave is cosine instead of sine
-//      if (j % 2 == 0)  yvalues[i] += sin(x)*amplitude[j];
-//      else yvalues[i] += cos(x)*amplitude[j];
-//      x+=dx[j];
-//    }
-//  }
-//}
-//
-//function renderWave() {
-//  // A simple way to draw the wave with an ellipse at each location -- yvalues.length --
-//  stroke(250);
-//  fill(255,50);
-////  ellipseMode(CENTER);
-//  for (var x = 0; x < 20; x++) {
-//    stroke(255);
-//    line(x*xspacing+50,height/2+yvalues[x],x*xspacing+50,-.500010);
-//    ellipse(x*xspacing+50,height/2+yvalues[x],30,30);
-//  }
-//  stroke(255);
-//  if (true) {
-//    line(mouseX, mouseY, pmouseX, pmouseY);
-//    //line();
-//  }
-//}
+function Seed() {
+	this.amplitude = 1;
+	this.period = random(90,100);
+    this.x = 225+seedOrigin;
+    this.y = 80;
+    this.display = function() {
+        noStroke();
+        fill(150,85);
+        ellipse(this.x, this.y, 20,20);
+	};
+	this.waveDown = function () {
+		this.y = this.y + this.amplitude * sin((frameCount/this.period)*TWO_PI);
+	};
+//	this.waveUp = function () {
+//		this.y = this.y - 1;
+//	};
+}
+ 
+
