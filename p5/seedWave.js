68123a91c0030c660e6a9d8e3733a39c65ef341c
CruzMendez <camendez@utexas.edu>
2018-10-22 19:40:54 -0500

    10-22-18 19:40
    seed updated and grid p5 added

--- p5/seedWave.js
+++ p5/seedWave.js
@@ -1,6 +1,6 @@
 var canvas;
 var seedCount = 15;
-var seedOrigin = 0;
+var seedOrigin = 40;
 var seedID = 0;
 var seeds = [];
 var showLine;
@@ -18,21 +18,21 @@ var fRate = 10;
 amp = 1.6
 
 
-var pageTitle = ["services", 
-				 "illustrations", 
-				 "modeling", 
-				 "interactive",  
-				 "projects", 
+var pageTitle = ["services",
+				 "illustrations",
+				 "modeling",
+				 "interactive",
+				 "projects",
 				 "photography",
 				 "resources",
 				 "about"];
 
-var pageSubTitle = ["WHAT I CAN DO", 
-					"2D GRAPHICS", 
-					"3D GRAPHICS", 
-					"INFO GRAPHICS", 
-					"WORK SAMPLES", 
-					"PHOTO GRAPHICS", 
+var pageSubTitle = ["WHAT I CAN DO",
+					"2D GRAPHICS",
+					"3D GRAPHICS",
+					"INFO GRAPHICS",
+					"WORK SAMPLES",
+					"PHOTO GRAPHICS",
 					"LIBRARY",
 					"ME"];
 var pageNumber = 0;
@@ -50,12 +50,14 @@ var homeRect;
 var cord2;
 var zIndex = 100;
 var newSeed;
+var connect;
+var randSeed;
 
 
 
 
 function setup() {
-    canvas = createCanvas(innerWidth,innerHeight); 
+    canvas = createCanvas(innerWidth,innerHeight);
     canvas.position(0,0);
 	frameRate(fRate);
 	for (var i=0; i<seedCount; i++) {
@@ -64,31 +66,36 @@ function setup() {
 		seedID++;
 	};
 
-	
+
 	title = createElement('h1','P5.js Canvas');
 	title.addClass('enter-name');
 	title.position(35,innerHeight-100);
-	
-	rateBtn = createButton("Increase Speed");
-	rateBtn.position(20,(innerHeight/2)-60);
+
+	rateBtn = createButton("Increase Rate");
+	rateBtn.position(20,(innerHeight/2)-50);
 	rateBtn.addClass('rate-btn');
 	rateBtn.mousePressed(rateUp);
-	
+
 	ampBtn = createButton("Increase Amplitude");
-	ampBtn.position(20,(innerHeight/2)-110);
+	ampBtn.position(20,(innerHeight/2)-130);
 	ampBtn.addClass('amp-btn');
 	ampBtn.mousePressed(ampUp);
 
-	// newSeed = createButton("Add Ellipse");
-	// newSeed.position(20,(innerHeight/2)-160);
-	// newSeed.addClass('amp-btn');
-	// newSeed.mousePressed(addSeed);
+	newSeed = createButton("Add Ellipse");
+	newSeed.position(20,(innerHeight/2)+27);
+	newSeed.addClass('add-btn');
+	newSeed.mousePressed(addSeed);
+
+	// connect = createElement('p','Click an ellipse to create a connection');
+	// connect.position(20,(innerHeight/2)+40);
+	// connect.addClass('connect-btn');
 };
 
+
 function addSeed() {
-	seeds[seedCount-1] = new Seed();
+	seeds[seedCount] = new NewSeed();
 	seedCount++;
-}
+};
 
 function rateUp() {
 	fRate = fRate * 2;
@@ -102,14 +109,14 @@ function ampUp() {
 };
 
 function windowResized() {
-	resizeCanvas(innerWidth, innerHeight);
+	resizeCanvas(windowWidth, windowHeight);
 };
 
-function mousePressed() {
-	for (var i=0; i < seeds.length; i++) {
-		seeds[i].newPage();
-	};
-};
+// function mousePressed() {
+// 	for (var i=0; i < seeds.length; i++) {
+// 		seeds[i].connection();
+// 	};
+// };
 
 
 function draw() {
@@ -125,7 +132,10 @@ function draw() {
 		seeds[i].display();
 		seeds[i].wave();
 		seeds[i].highlight();
-	}; 	
+		// seeds[i].connection();
+	};
+	// stroke(random(0,255),random(0,255),random(0,255));
+	// line(random(0,innerWidth),random(0,innerHeight),mouseX,mouseY);
 };
 
 
@@ -135,24 +145,17 @@ function Seed() {
 	this.inDex = 0 + seedID;
 	this.amplitude = amp;
 	this.period = random(150,155);
-    this.x = (innerWidth/2-(160))+seedOrigin;
-    this.y = (innerHeight/2) - (50);
+  this.x = 275 + seedOrigin;
+  this.y = (innerHeight/2) - (50);
 	this.diam = 20;
 	this.seedStroke = 63;
-//	this.cord1 = undefined;
-//	this.cord3 = undefined;
-    this.display = function() {		
+  this.display = function() {
         stroke(this.seedStroke);
-        fill(150,95);
+        fill(10,129,147);
         ellipse(this.x, this.y, this.diam, this.diam);
 		stroke(255);
-//		line(this.x, innerHeight-130, this.cord3, this.y);
-//		line(seeds[0].x, seeds[0].y, seeds[0].x, showLine);
-//		line(20, innerHeight-130, this.cord3, innerHeight-130);
 		fill(0,0);
 		stroke(underline, 40)
-		// ellipse(seeds[0].x,seeds[0].y,20,20);
-//		this.cord1 = line(20, innerHeight-130, seeds[0].x, innerHeight-130);
 	};
 	this.wave = function () {
 		this.y = this.y + this.amplitude * sin((frameCount/this.period)*TWO_PI);
@@ -165,42 +168,44 @@ function Seed() {
 			ellipse(this.x, this.y, 20, 20);
 		};
 	};
-	this.newPage = function() {
+	// this.connection = function () {
+	// 	var s = random(0,seedCount);
+	// 	var d = dist(mouseX,mouseY, this.x, this.y);
+	// 	if (d < 14) {
+	// 		stroke(200);
+	// 		line(seeds[s].x,seeds[s].y,seeds[s].x,seeds[s].y);
+	// 	};
+	// };
+};
+
+// NEW SEED ADDITION
+
+function NewSeed() {
+	seedOrigin = seedOrigin + 25;
+	this.inDex = 0 + seedID;
+	this.amplitude = amp;
+	this.period = random(150,155);
+    this.x = 275+seedOrigin;
+    this.y = (innerHeight/2) - (50);
+	this.diam = 20;
+	this.seedStroke = 63;
+    this.display = function() {
+        stroke(this.seedStroke);
+        fill(10,129,147);
+        ellipse(this.x, this.y, this.diam, this.diam);
+		stroke(255);
+		fill(0,0);
+		stroke(underline, 40)
+	};
+	this.wave = function () {
+		this.y = this.y + this.amplitude * sin((frameCount/this.period)*TWO_PI);
+	};
+	this.highlight = function() {
 		var d = dist(mouseX,mouseY, this.x, this.y);
 		if (d < 14) {
-			
-			divServ = select('#services');
-			divServ2 = select('#services-behind');
-			divServ.addClass('div-hide');
-			divServ2.addClass('div-hide');
-	
-		
-			for (var i = 0; i < seeds.length; i++) {
-				this.seedStroke = 255;
-				seeds[i].cord3 = undefined;
-				this.cord3 = this.x;
-				pageNumber = this.inDex;
-				titleText.addClass('text-hide');
-				titleSubText.addClass('text-hide');
-	//				page0();
-				showLine = undefined;
-				if (this.inDex == 0) {
-					page0();
-				};
-				if (this.inDex == 1) {
-					page1();
-				};
-				if (this.inDex == 2) {
-					page2();
-				};
-				if (this.inDex == 3) {
-					page3();
-				};
-				if (this.inDex == 4) {
-					page4();
-				};
-			};
+			noStroke();
+			fill(150,250);
+			ellipse(this.x, this.y, 20, 20);
 		};
 	};
 };
-
